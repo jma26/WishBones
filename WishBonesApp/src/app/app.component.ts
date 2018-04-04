b93d81c81eff5b38ca5c115894992019e8333dd9
jma26 <j.masonoma17@gmail.com>
2018-04-04 01:58:15 -0700

    project name changed, updated package.json dependencies, updated ng root route to contain login and registration

--- WishBonesApp/src/app/app.component.ts
+++ WishBonesApp/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+  newUser: any;
+  user: any;
+
+  constructor() { }
+
+  ngOnInit() {
+    // 2-way binding
+    this.newUser = {
+      alias: '',
+      fname: '',
+      lname: '',
+      email: '',
+      password: '',
+      passwordconfirm: ''
+    }
+    this.user = {
+      email: '',
+      password: ''
+    }
+   }
+
 }
